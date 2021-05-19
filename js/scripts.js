3aa20472a9fa835d7cc0bc1c054af30bcb821dfe
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-05-19 07:53:29 +0200

    Update scripts.js
    Deleted previous code and added new code with pokemonlist.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-alert('Hello world');
-let favoriteFood = 'Banana';
-document.write(favoriteFood);
+let pokemonList = [{name: “Bulbasaur”, height: 7, types: ["grass", "poison"]},
+          {name: "Ivysaur", height: 8, types: ["grass", "poison"]},
+          {name: "Charizard" , height: 10, types: ["fire","flying"]}];
