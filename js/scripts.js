066e08660f0bcb79155fd4a1a8f05280c9786aa2
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-05-20 22:02:00 +0200

    Update scripts.js
    Updated the names of the Pokemon.
    Included a loop to write the name of Pokemon and to compare their height to find the tallest one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-let pokemonList = [{name: “Bulbasaur”, height: 7, types: ["grass", "poison"]},
-          {name: "Ivysaur", height: 8, types: ["grass", "poison"]},
-          {name: "Charizard" , height: 10, types: ["fire","flying"]}];
+let pokemonList = [
+          {name: "Squirtle", height: 1, types: ["water"]},
+          {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
+          {name: "Charmander", height: .6, types: ["fire"]}
+          ];
+
+           //to write the name of the pokemon.
+          for(let i=0; i < pokemonList.length; i++){
+
+              //to find out the tallest pokemon
+            if (pokemonList[i].height >1){
+              document.write(`Pokemon Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height}) WOW !! It\'s Big!`);
+              document.write("<br>");
+              document.write("<br>");
+              }
+
+              else {
+
+            document.write(`Pokemon Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height})`);
+            document.write("<br>");
+            document.write("<br>");
+          }
+}
