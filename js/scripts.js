167e6a54e221ac52304140a4a41fc04e9f3a2081
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-05-25 12:31:12 +0200

    Update scripts.js
    'for' loop is replace with 'forEach' loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,19 @@
-let pokemonList = [
-          {name: "Squirtle", height: 1, types: ["water"]},
-          {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
-          {name: "Charmander", height: .6, types: ["fire"]}
-          ];
+  let pokemonList = [
+      {name: "Squirtle", height: 1, types: ["water"]},
+      {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
+      {name: "Charmander", height: .6, types: ["fire"]}
+  ];
 
-           //to write the name of the pokemon.
-          for(let i=0; i < pokemonList.length; i++){
-
-              //to find out the tallest pokemon
-            if (pokemonList[i].height >1){
-              document.write(`Pokemon Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height}) - Wow, that\'s big!`);
-              document.write("<br>");
-              document.write("<br>");
-              }
-
-              else {
-
-            document.write(`Pokemon Name: ${pokemonList[i].name} (Height: ${pokemonList[i].height})`);
-            document.write("<br>");
-            document.write("<br>");
-          }
-}
+       //to write the name of the pokemon.
+  pokemonList.forEach(function(pokemon){
+      //to find out the tallest pokemon
+      if (pokemon.height >1){
+      document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height}) - Wow, that\'s big!`);
+      document.write("<br>");
+      document.write("<br>");
+      }else {
+      document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height})`);
+      document.write("<br>");
+      document.write("<br>");
+      }
+  });
