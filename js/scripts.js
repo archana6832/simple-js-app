224d9fd91b6fe829ccb8271fa5f63a70b24eab84
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-05-25 13:08:34 +0200

    Update scripts.js
    Changed the parameter name  and done code formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
+// To IIFE to avoid accidentally accessing the global state.
+let pokemonRepository = (function () {
   let pokemonList = [
-      {name: "Squirtle", height: 1, types: ["water"]},
-      {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
-      {name: "Charmander", height: .6, types: ["fire"]}
+    {name: "Squirtle", height: 1, types: ["water"]},
+    {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
+    {name: "Charmander", height: .6, types: ["fire"]}
   ];
 
-       //to write the name of the pokemon.
-  pokemonList.forEach(function(pokemon){
-      //to find out the tallest pokemon
-      if (pokemon.height >1){
-      document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height}) - Wow, that\'s big!`);
-      document.write("<br>");
-      document.write("<br>");
-      }else {
-      document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height})`);
-      document.write("<br>");
-      document.write("<br>");
-      }
-  });
+  function getAll(){
+    return pokemonList;
+  }
+  function add(item){
+    pokemonList.push(item);
+  }
+  return{
+    getAll: getAll,
+    add: add
+  }
+})();
+
+  //to write the name of the pokemon.
+pokemonRepository.getAll().forEach(function(pokemon){
+  //to find out the tallest pokemon
+  if (pokemon.height >1){
+    document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height}) - Wow, that\'s big!`);
+    document.write("<br>");
+    document.write("<br>");
+  }else {
+    document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height})`);
+    document.write("<br>");
+    document.write("<br>");
+  }
+});
