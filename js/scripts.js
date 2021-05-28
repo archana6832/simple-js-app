79a40021583c051dce57910aad9a5b61852c7b35
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-05-28 16:41:46 +0200

    Update scripts.js
    Changed the variable name pokemonList to repository . Added new functions addListItem to show buttons for pokemon names. Added evenhandlers with each buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,57 @@
 // To IIFE to avoid accidentally accessing the global state.
 let pokemonRepository = (function () {
-  let pokemonList = [
+  let repository = [
     {name: "Squirtle", height: 1, types: ["water"]},
     {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
     {name: "Charmander", height: .6, types: ["fire"]}
   ];
 
   function getAll(){
-    return pokemonList;
+    return repository;
   }
-  function add(item){
-    pokemonList.push(item);
+  //to add new pokemons in the repository
+  function add(pokemon) {
+     if (
+       typeof pokemon === "object" &&
+       "name" in pokemon &&
+       "height" in pokemon &&
+       "types" in pokemon
+     ) {
+       repository.push(pokemon);
+     } else {
+       console.log("pokemon is not correct");
+     }
+   }
+
+   //to Create buttons  for pokemons in the webpage
+  function  addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    //to add an event listener to the button just above created
+    button.addEventListener('click', function (event) {
+    showDetails(pokemon);
+    });
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
   }
+  //to show the details of the pokemon selected with buttons
+  function showDetails(pokemon){
+   console.log(pokemon);
+  }
+// to return all the required variables
   return{
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem,
+    showDetails: showDetails
   }
 })();
 
-  //to write the name of the pokemon.
+  // to work with each item in the repository
 pokemonRepository.getAll().forEach(function(pokemon){
-  //to find out the tallest pokemon
-  if (pokemon.height >1){
-    document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height}) - Wow, that\'s big!`);
-    document.write("<br>");
-    document.write("<br>");
-  }else {
-    document.write(`Pokemon Name: ${pokemon.name} (Height: ${pokemon.height})`);
-    document.write("<br>");
-    document.write("<br>");
-  }
+  //to write the name of the pokemon on the buttons
+  pokemonRepository.addListItem(pokemon);
 });
