3b260113b335e615ffc7165d9493635ca2e316dd
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-06-01 18:15:40 +0200

    Update scripts.js
    Changed static pokemon list with pokemonlist api. Added new function loadlist and loaddetails to get the pokemon list and details from api

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,23 @@
-// To IIFE to avoid accidentally accessing the global state.
+// To create  IIFE to avoid accidentally accessing the global state.
 let pokemonRepository = (function () {
-  let repository = [
-    {name: "Squirtle", height: 1, types: ["water"]},
-    {name: "Charizard" , height: 1.7, types: ["fire","flying"]},
-    {name: "Charmander", height: .6, types: ["fire"]}
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function getAll(){
-    return repository;
+    return pokemonList;
   }
   //to add new pokemons in the repository
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
       "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
-     ) {
-      repository.push(pokemon);
-     } else {
+      "detailsUrl" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
       console.log("pokemon is not correct");
-     }
-   }
+    }
+  }
 
   //to Create buttons  for pokemons in the webpage
   function  addListItem(pokemon){
@@ -38,21 +34,61 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(listpokemon);
   }
 
-  //to show the details of the pokemon selected with buttons
-  function showDetails(pokemon){
-    console.log(pokemon);
+  //     load the List of the pokemons from API (A promise function)
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
+  //to load the details of the pokemos from api
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+  //to show the details of the pokemon selected with buttons
+  function showDetails(pokemon) {
+    //to load the pokemon details from api
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
   // to return all the required variables
   return{
     getAll: getAll,
     add: add,
     addListItem: addListItem,
     showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails
   }
 })();
 
 // to work with each item in the repository
-pokemonRepository.getAll().forEach(function(pokemon){
-  //to write the name of the pokemon on the buttons
-   pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
